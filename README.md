9a105f9d8aa6b4d5238fb152392129019dd60e23
Piotr Pasterak <piotr.pasterak@aptiv.com>
2019-05-24 12:44:12 +0200

    Update

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 # RocketOptimizer
 
+##Problem
 Problem, który jest poruszony w niniejszej pracy dotyczy rozważań nad konstrukcją pierwszego członu rakiety nośnej Saturn V. Rakieta ta jest jednym z największych i najpotężniejszych obiektów kosmicznych wyprodukowanych w dziejach ludzkości. W 1969 roku wyniosła na orbitę i pozwoliła misji Apollo wylądować na księżycu oraz powrócić z niego.
 Mimo tak odpowiedzialnego zadania, cały obiekt jest połączeniem zarówno prostoty jak i bezpieczeństwa. Jest to wynikiem braków technologicznych tamtych czasów, presji czasu związanej z wyścigiem o podbój kosmosu oraz niezwykłej współpracy tysięcy naukowców i inżynierów. Postawienie na prostotę skutkuje tym, że opis fizyczny pracy pierwszego stopnia rakiety daje się wyrazić za pomocą kilku zmiennych i kilkunastu stałych. Jednak prawdziwym zadaniem jest określenie, jak ma wyglądać optymalna praca tego podzespołu.
 
+##Wybrane Algorytmy optymalizacji
+Wybrano dwa algorytmy optymalizacji nieliowej z ograniczeniami:
+Metoda COBYLA która jest najbardziej preferowana ze wzgledu na mechanism przyblizania przez linearyzowanie.
+Metodę trust-constr ktora wyznacza minimum bazujac na gradiencie.
+
+Przyjęte ograniczenia wynikuja z wlasciwości fizycznych (średnica musi być dodatnia),
+oraz z wlasciwosci funkcji celu (wartości rzeczywiste dla V)
+
+##Rozwiazanie
+przedstwawione w postaci x(R,r)
+
+Dla metody COBYLA
+fun: 3660.0713149354438
+x: array([99.91415487,  0.6065698])
+
+Dla metody trust-constr
+fun: 209129.62009804428
+x: array([99.25333925,  1.86799574])
+
+Uzyskane wyniki wskazuja ze algorytm COBYLA(Constrained Optimization BY Linear Approximation) jest lepsza metoda dla tego problemu.
+Także czasowo algorytm COBYLA generuje wyniki szybciej.
+Wynika to z charakteru funkcji celu która jest mocno liniowa w znacznacej czesci swojej dzedziny.
+
+Ostatecznie:
+r = 0.6065698
+R = 99.91415487
+V = 3660.0713149354438
--- RocketOptimizer.py
+++ RocketOptimizer.py
@@ -12,8 +12,8 @@ def goal_f(r):
     return y_1 * r[0]**2 * r[1]**3 * (y_2 + np.sqrt(y_2**2 - (y_3 / (r[0]**2 * r[1]**3))))
 
 
-def restrict_f(r):
-    return np.sqrt(y_2**2 - (y_3 / (r[0]**2 * r[1]**3)))
+def restrict_f(x):
+    return y_2**2 - (y_3 / (x[0]**2 * x[1]**3))
 
 
 def print_plot():
@@ -23,33 +23,55 @@ def print_plot():
     X = np.arange(10, 40000, 100)
     Y = np.arange(3, 40, 1)
     X, Y = np.meshgrid(X, Y)
-    plt.title('Funkcja celu')
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    ax.set_zlabel("Z")
+    plt.title('Funkcja celu rakiety Saturn V')
+    ax.set_xlabel("R")
+    ax.set_ylabel("r")
+    ax.set_zlabel("V")
     Z = goal_f([X, Y])
 
     ax.plot_surface(X, Y, Z, linewidth=0, antialiased=True)
-
-    #ax.plot_surface(X, Y, restrict_f([X, Y]), linewidth=0, antialiased=True)
-
     plt.show()
+
 
 if __name__ == '__main__':
     """Main script loop.
+    Zadanie optymalizacyjne:
+    Promień dyszy silnika, oznaczany małą literą r,
+    Promień zewnętrzny stopnia rakiety, oznaczany dużą literą R.
     """
-    bnds = ((0.1, 0.001), (1000, 100))
 
-    cons = [{'type': 'ineq', 'fun': lambda x: y_2**2 - (y_3 / (x[0]**2 * x[1]**3))},
+    cons = [{'type': 'ineq', 'fun': restrict_f},
             {'type': 'ineq', 'fun': lambda x: x[0]},
             {'type': 'ineq', 'fun': lambda x: x[1]}]
 
-    fopt = optimize.minimize(goal_f, [100, 10],method="trust-constr", constraints=cons, hess=None)
-    print(fopt)
+    print(
+        """
+        Method COBYLA uses the Constrained Optimization BY Linear Approximation (COBYLA) method. 
+        The algorithm is based on linear approximations to the objective function and each constraint. 
+        The method wraps a FORTRAN implementation of the algorithm. 
+        The constraints functions ‘fun’ may return either a single number or an array or list of numbers.
+        """
+    )
 
-    print("----------------------------------------")
+    result_cobyla = optimize.minimize(goal_f, [100, 10],method="COBYLA", constraints=cons)
+    print(result_cobyla)
 
-    fopt2 = optimize.minimize(goal_f, [100, 10],method="COBYLA", constraints=cons)
-    print(fopt2)
+    print(
+        """
+        Method trust - constr is a trust - region algorithm for constrained optimization.
+        It swiches between two implementations depending on the problem definition.
+        It is the most versatile constrained minimization algorithm implemented in SciPy and the most appropriate for large-scale problems.
+        This interior point algorithm, in turn, solves inequality constraints by introducing slack variables and solving a sequence of equality-constrained barrier problems for progressively smaller values of the barrier parameter.
+        The previously described equality constrained SQP method is used to solve the subproblems with increasing levels of accuracy as the iterate gets closer to a solution.
+    
+        Function plot suggesting that function is close to be linear, at least close to this in significant number of regions.
+        That could means that 'trust-constr' method is not best choice for this case (bases on gradient and is time consuming here.).
+        
+        It takes long time, please be waiting.
+        """)
+
+    result_trust = optimize.minimize(goal_f, [100, 10], jac='2-point', method="trust-constr", constraints=cons,
+                             options={'maxiter': 100000})
+    print(result_trust)
 
     print_plot()
